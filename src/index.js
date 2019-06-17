c9f530226388f4a8ca23d804fff38292d8eeab51
rebecademelo <rebeca_mss@hotmail.com>
2019-06-17 01:28:18 -0300

    instagram web - frontend

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+import './global.css';
+
+//ReactDOM - integração do react com o navegador
+
+ReactDOM.render(<App />, document.getElementById('root'));
